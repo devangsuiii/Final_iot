25b9968657ad830f1821ad720156d7dc33906d3c
devangsuiii <130946505+devangsuiii@users.noreply.github.com>
2024-03-27 09:17:57 +0530

    Add files via upload
    Formatted documentation

---
+++ Final_iot/iot_configs.h
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "MSI 7403"
+#define IOT_CONFIG_WIFI_PASSWORD "12345678"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "iotAIr.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "esp8266"
+#define IOT_CONFIG_DEVICE_KEY "A08Z66NH/DeIlVdNqvFt6kozg4AKYOJaJAIoTPYMI08="
+
+// Publish 1 message every 2 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 2000
